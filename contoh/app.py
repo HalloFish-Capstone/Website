ac06ee60c1674d65451df6df0ec3c5fea28117ae
Ahmad Akmal <99625730+ahmadakmal01@users.noreply.github.com>
2023-06-15 14:14:13 +0800

    Update app.py

--- contoh/app.py
+++ contoh/app.py
@@ -1,20 +1,25 @@
+import keras.utils as image
+import numpy as np
+import tensorflow as tf
+from PIL import Image
 from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
 
-app = Flask(__name__)
+app = Flask(__name__)#masih pakai flash
 
 dic = {0 : 'WHITESPOT', 1 : 'BACTERIALFIN', 2 : 'REDSPOT', 3 : 'HEALTHY'}
 
-model = load_model('model.h5')
+model = load_model('pat/model.h5')#ganti path yg sesuai
 
 model.make_predict_function()
 
 def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(100,100))
+	i = tf.keras.utils.load_img(img_path, target_size=(224,224))
 	i = image.img_to_array(i)/255.0
-	i = i.reshape(1, 100,100,3)
-	p = model.predict_classes(i)
+	i = i.reshape(1, 224, 224, 3)
+	p = np.argmax(model.predict(i),axis=1)
 	return dic[p[0]]
 
 
@@ -31,7 +36,6 @@ def about_page():
 def get_output():
 	if request.method == 'POST':
 		img = request.files['my_image']
-
 		img_path = "static/" + img.filename	
 		img.save(img_path)
 
